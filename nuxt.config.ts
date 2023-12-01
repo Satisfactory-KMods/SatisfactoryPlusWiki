faee158282d54d9b2b27dc1ad4a2bda9b4cee7ba
Kaufmann, Oliver (SI E SCM LPZ MF) <kaufmann.oliver@siemens.com>
2023-12-01 14:44:30 +0100

    chore: update tsconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,37 +2,37 @@ import { env } from './env.mjs';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: [
-		'@vueuse/nuxt',
-		'@pinia/nuxt',
-		'@formkit/auto-animate',
-		'@pinia-plugin-persistedstate/nuxt',
-		'nuxt-typed-router',
-		'@nuxt/ui',
-		'nuxt-lodash',
-		'nuxt-icon',
-		'nuxt3-leaflet',
-		'@hebilicious/authjs-nuxt',
-		'@vue-final-modal/nuxt',
-		'@nuxt/image'
-	],
-	devtools: { enabled: true },
-	alias: {
-		cookie: 'cookie'
-	},
-	runtimeConfig: {
-		authJs: {
-			secret: process.env.AUTH_SECRET // You can generate one with `openssl rand -base64 32`
-		},
-		discord: {
-			clientId: env.AUTH_DISCORD_CLIENT_ID,
-			clientSecret: env.AUTH_DISCORD_CLIENT_SECRET
-		},
-		public: {
-			authJs: {
-				baseUrl: env.AUTH_DEPLOY_URL,
-				verifyClientOnEveryRequest: true
-			}
-		}
-	}
+								modules: [
+																'@vueuse/nuxt',
+																'@pinia/nuxt',
+																'@formkit/auto-animate',
+																'@pinia-plugin-persistedstate/nuxt',
+																'nuxt-typed-router',
+																'@nuxt/ui',
+																'nuxt-lodash',
+																'nuxt-icon',
+																'nuxt3-leaflet',
+																'@hebilicious/authjs-nuxt',
+																'@vue-final-modal/nuxt',
+																'@nuxt/image'
+								],
+								devtools: { enabled: true },
+								alias: {
+																cookie: 'cookie'
+								},
+								runtimeConfig: {
+																authJs: {
+																								secret: process.env.AUTH_SECRET // You can generate one with `openssl rand -base64 32`
+																},
+																discord: {
+																								clientId: env.AUTH_DISCORD_CLIENT_ID,
+																								clientSecret: env.AUTH_DISCORD_CLIENT_SECRET
+																},
+																public: {
+																								authJs: {
+																																baseUrl: env.AUTH_DEPLOY_URL,
+																																verifyClientOnEveryRequest: true
+																								}
+																}
+								}
 });
