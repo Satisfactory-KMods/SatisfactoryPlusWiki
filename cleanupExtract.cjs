98b0504db456e71eae276abfb6775765e14f6a68
Kyrium <olli143@live.de>
2023-12-22 14:34:20 +0100

    chore: update gamefiles

--- cleanupExtract.cjs
+++ cleanupExtract.cjs
@@ -29,12 +29,10 @@ async function read(path) {
 		if (
 			item.isFile() &&
 			!item.name.toLowerCase().endsWith('.json'.toLowerCase()) &&
-			(item.name.toLowerCase().startsWith('UI_'.toLowerCase()) ||
-				item.name.toLowerCase().startsWith('T_'.toLowerCase()) ||
-				item.name.toLowerCase().startsWith('MI_'.toLowerCase()) ||
-				item.name.toLowerCase().startsWith('TXUI_'.toLowerCase()) ||
-				item.name.toLowerCase().startsWith('tex_'.toLowerCase()) ||
-				item.name.toLowerCase().endsWith('_AO.png'.toLowerCase()) ||
+			//	item.name.toLowerCase().startsWith('T_'.toLowerCase()) ||
+			//	item.name.toLowerCase().startsWith('MI_'.toLowerCase()) ||
+			//	item.name.toLowerCase().startsWith('tex_'.toLowerCase()) ||
+			(item.name.toLowerCase().endsWith('_AO.png'.toLowerCase()) ||
 				item.name.toLowerCase().endsWith('_AOMasks.png'.toLowerCase()) ||
 				item.name.toLowerCase().endsWith('_ORMA.png'.toLowerCase()) ||
 				item.name.toLowerCase().endsWith('_MREA.png'.toLowerCase()) ||
@@ -73,7 +71,7 @@ async function read(path) {
 			const img = await Jimp.read(join(path, item.name)).catch(() => null);
 			if (!img) continue;
 			await fs.unlink(join(path, item.name));
-			if (img.bitmap.width === img.bitmap.height && img.bitmap.width <= 1024) {
+			if (img.bitmap.width === img.bitmap.height && img.bitmap.width <= 2048) {
 				if (img.bitmap.width > 256) {
 					console.log('resize', join(path, item.name));
 					await img.resize(256, 256).quality(80).writeAsync(join(path, item.name));
