235dc113e7a194c2b40b967aeadf3068fc091337
Kyri123 <olli143@live.de>
2024-02-19 07:23:05 +0100

    ci: use script to download pngs

--- installPngs.sh
+++ installPngs.sh
@@ -1,1 +1,7 @@
-wget 
+echo "Removing old pngs"
+rm -rf public/sf/**/*.png
+echo "Downloading and extracting wiki.tar"
+wget https://kmods.space/static/wiki.tar
+tar -xvf wiki.tar
+rm wiki.tar
+echo "Done"
