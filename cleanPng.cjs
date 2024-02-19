9ba9798685f1592fce46344cb15fbfb167bbc247
Kyrium <olli143@live.de>
2024-02-19 06:43:32 +0100

    chore: update public folder and add some more informations

---
+++ cleanPng.cjs
@@ -0,0 +1,17 @@
+/* eslint-disable */
+const fs = require('fs/promises');
+const { join } = require('path');
+
+async function cleanup(path) {
+	const scanResult = await fs.readdir(path, { withFileTypes: true });
+	for (const item of scanResult) {
+		if (item.isFile() && item.name.toLowerCase().endsWith('.png'.toLowerCase())) {
+			console.log('removing', join(path, item.name));
+			await fs.unlink(join(path, item.name));
+		} else if (item.isDirectory()) {
+			await cleanup(join(path, item.name));
+		}
+	}
+}
+
+cleanup(join(process.cwd(), 'public/sf')).then(() => {});
